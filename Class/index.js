9a3e02a29f761e6bd0959aa08dedf9b971f5aef6
ishalsey <hal17038@byui.edu>
2020-09-19 10:59:37 -0600

    error

--- Class/index.js
+++ Class/index.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', (event)=>{
     const lu = document.querySelector("#lastupdated");
-    lu.testContent = document.lastModified;
+    lu.textContent = document.lastModified;
 
     const cry = document.querySelector("#copyrightyear");
     cry.textContent = new Date().getFullYear();
