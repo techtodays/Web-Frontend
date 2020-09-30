3609f1082fccbc9fa40fe3e7ff792293bf020660
ishalsey <hal17038@byui.edu>
2020-09-29 22:38:49 -0600

    t

--- class/index.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', (event)=>{
-    const lu = document.querySelector("#lastupdated");
-    lu.textContent = document.lastModified;
-
-    const cry = document.querySelector("#copyrightyear");
-    cry.textContent = new Date().getFullYear();
-  })
