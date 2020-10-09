5763ec9bc607f20a1b5ac0dfc0c015f1a7efd147
ishalsey <hal17038@byui.edu>
2020-10-08 20:40:35 -0600

    n

--- lesson4/preston-4.js
+++ lesson4/preston-4.js
@@ -1,5 +1,2 @@
-const menubutton = document.querySelector('.menu');
-const mainnav = document.querySelector('.nav')
-
-menubutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+var mainnav = document.getElementById("navigation");
+mainnav.classList.toggle("responsive");
