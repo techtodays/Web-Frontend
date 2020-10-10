d627ae00f9ff7ae76494ce54513216c9b9b3ed00
ishalsey <hal17038@byui.edu>
2020-10-10 13:12:52 -0600

    new

--- lesson4/preston-4.js
+++ lesson4/preston-4.js
@@ -4,3 +4,10 @@ var newbutton = document.querySelector(".ham");
 newbutton.addEventListener("click", function(){
 mainnav.classList.toggle("responsive")},false);
 });
+window.addEventListener('load', (event)=>{
+    const lu = document.querySelector("#lastupdated");
+    lu.textContent = document.lastModified;
+
+    const cry = document.querySelector("#copyrightyear");
+    cry.textContent = new Date().getFullYear();
+  })
