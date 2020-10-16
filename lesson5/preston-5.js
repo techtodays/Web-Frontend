205e7f1519736a7701d3cdf9f18014270dc39eee
ishalsey <hal17038@byui.edu>
2020-10-16 12:39:54 -0600

    yes

---
+++ lesson5/preston-5.js
@@ -0,0 +1,19 @@
+window.addEventListener("load",(event)=>{
+    var mainnav = document.querySelector(".navigation");
+    var newbutton = document.querySelector(".ham");
+    newbutton.addEventListener("click", function(){
+    mainnav.classList.toggle("responsive")},false);
+    const mb = document.querySelector('#new');
+        today = new Date().getDay();
+        if(today==5){
+          mb.style.display = 'block';
+        }
+    });
+    window.addEventListener('load', (event)=>{
+        const lu = document.querySelector("#lastupdated");
+        lu.textContent = document.lastModified;
+    
+        const cry = document.querySelector("#copyrightyear");
+        cry.textContent = new Date().getFullYear();
+      });
+    
