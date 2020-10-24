249955a7b61c067fbec72c4db60312cb87c351f4
ishalsey <hal17038@byui.edu>
2020-10-24 13:20:28 -0600

    pic update

--- lesson6/windchill.js
+++ lesson6/windchill.js
@@ -4,5 +4,11 @@ var t = document.getElementById("temp").innerHTML;
 t = parseInt(t);
 s = parseInt(s);
 var f = document.getElementById("wind");
-f.innerHTML = (35.74+0.6215*t-35.75*Math.pow(s,0.16)+0.4275*t*Math.pow(s,0.16)).toFixed(2);
-console.log('#wind')});
+var chill = (35.74+0.6215*t-35.75*Math.pow(s,0.16)+0.4275*t*Math.pow(s,0.16)).toFixed(2);
+if(t < 50 && s > 3 ){
+    f.textContent = Math.round(chill, 2);
+}
+else{
+    f.textContent = "N/A"
+}
+});
