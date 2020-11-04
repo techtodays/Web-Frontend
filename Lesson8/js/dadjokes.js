1d23e8db57eedd8fc86937532f9ae68cfee0dec8
ishalsey <hal17038@byui.edu>
2020-11-04 16:42:57 -0700

    u

--- Lesson8/js/dadjokes.js
+++ Lesson8/js/dadjokes.js
@@ -1,5 +1,5 @@
 async function getDadJoke(){
-    const url = 'https://icanhazdadjoke.com'
+    const url = 'https://icanhazdadjoke.com/search?term=hipster'
     let h = new Headers({
         'Accept': 'application/json',
         'User-Agent': 'WDD-230 Test User Agent https://ishalsey.github.io'
@@ -15,8 +15,18 @@ async function getDadJoke(){
 function newJoke(){
     const dad_id = document.getElementById('dad-joke');
     dad_id.innerHTML = '';
+    dad_id.classList.remove("small");
     const joke = getDadJoke()
     .then(function(j){
-        dad_id.innerHTML = j['joke'];
+        dad_id.innerHTML = j['results'][1]['joke'];
+        if(j['results'][1]['joke'].length > 85){
+            dad_id.classList.add("small");
+        }
     })
+    .catch(function(e){
+        dad_id.innerHTML = e;
+    });
 }
+window.addEventListener('load',()=>{
+    newJoke();
+})
