53b5889123885198d9970ffeec66f578c4f57b39
ishalsey <hal17038@byui.edu>
2020-11-25 16:30:58 -0700

    yes

--- lesson11/js/town.js
+++ lesson11/js/town.js
@@ -8,7 +8,6 @@ fetch(requestURL)
     const town = jsonObject['towns'];
     for (let i = 0; i < town.length; i++ ){
         if (town[i].name == "Fish Haven" || town[i].name == "Preston" || town[i].name == "Soda Springs"){
-            let section = document.createElement('section');
             let div = document.createElement('div');
             let h2 = document.createElement('h2');
             let italic = document.createElement('i');
@@ -18,21 +17,20 @@ fetch(requestURL)
             let p3 = document.createElement('p');
             let image = document.createElement('img');
             
-            div.appendChild(section);
             h2.textContent = town[i].name;    
-            section.appendChild(h2);
+            div.appendChild(h2);
             italic.textContent = town[i].motto;
             p0.appendChild(italic);
-            section.appendChild(p0);
+            div.appendChild(p0);
             p1.textContent = "Year Founded:" + town[i].yearFounded;
-            section.appendChild(p1);
+            div.appendChild(p1);
             p2.textContent = "Population:" + town[i].currentPopulation;
-            section.appendChild(p2);
+            div.appendChild(p2);
             p3.textContent = "Rainfall:" + town[i].averageRainfall;
-            section.appendChild(p3);
+            div.appendChild(p3);
             image.setAttribute("src", town[i].photo);
             div.appendChild(image);
-            document.querySelector('main.box').appendChild(div); 
+            document.querySelector('.box').appendChild(div); 
         }
     }
   });
