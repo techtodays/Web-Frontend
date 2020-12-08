1a4bcaef9cd72b341f0d89965d6471cecd22930f
ishalsey <hal17038@byui.edu>
2020-12-08 13:49:25 -0700

    m

--- temple_inn/js/laie_hawaii.js
+++ temple_inn/js/laie_hawaii.js
@@ -1,9 +1,11 @@
-const apiURL = '/temple_inn/js/temple.json';
+const apiURL = 'https://ishalsey.github.io/temple_inn/js/temple.json';
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    const temples = jsObject('temples')
-    document.getElementById(tname).textContent = temples[0].name;
-
-  })
+    const temples = jsObject['temples'];
+    document.getElementById(temple_name).textContent = temples[0].name;
+    document.getElementById(taddress).textContent = temples[0].address;
+    document.getElementById(tnumber).textContent = temples[0].number;
+    document.getElementById(temail).textContent = temples[0].email;
+  });
