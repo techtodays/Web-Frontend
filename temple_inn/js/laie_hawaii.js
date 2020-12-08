4b0e391af9528e7272abd3266bb2185216880564
ishalsey <hal17038@byui.edu>
2020-12-08 13:27:19 -0700

    uye

---
+++ temple_inn/js/laie_hawaii.js
@@ -0,0 +1,8 @@
+const apiURL = '/temple_inn/js/temple.json';
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById(tname).textContent = jsObject[0].name;
+
+  })
