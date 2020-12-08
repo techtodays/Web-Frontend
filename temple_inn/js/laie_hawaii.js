ec723b50d0b62a8bc993ef7d66027c6b461f7b1c
ishalsey <hal17038@byui.edu>
2020-12-08 14:25:48 -0700

    q

--- temple_inn/js/laie_hawaii.js
+++ temple_inn/js/laie_hawaii.js
@@ -3,8 +3,8 @@ fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    const new_temples = jsObject['new_temples'];
-    document.getElementById(tname).textContent = new_temples[0].name;
+    const temples = jsObject['new_temples'];
+    document.getElementById(tname).textContent = temples[0].name;
     document.getElementById(taddress).textContent = temples[0].address;
     document.getElementById(tnumber).textContent = temples[0].number;
     document.getElementById(temail).textContent = temples[0].email;
